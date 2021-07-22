21f44d989a6779ec16ba94d19d691606acc563cd
PozNas1 <anastasia.pazniak@gmail.com>
2021-07-22 14:10:28 +0300

    Adjust component: delete all unuseful tags.

--- src/components/Gifs.vue
+++ src/components/Gifs.vue
@@ -1,20 +1,5 @@
 <template>
   <v-app class="inspire">
-    <v-app-bar
-      app
-      shrink-on-scroll
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Gifs</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
       <v-container>
         <v-row>
@@ -24,8 +9,7 @@
             cols="4"
           >
             <v-card height="250" width="370">
-              <!-- <div> {{gif.link}}</div> -->
-              <img :src="gif.link" alt="Can't load" height="250" width="370">
+              <v-img :src="gif.link" height="250" width="370"></v-img>
             </v-card>
           </v-col>
         </v-row>
@@ -36,11 +20,12 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
-import {GifObj} from '../App.vue';
+import {GifObj} from '../views/Home.vue';
 
-@Component
+@Component({
+  name: 'Gifs'
+})
 export default class Gifs extends Vue {
-  @Prop() private msg!: string;
   @Prop({ type: Array, default: [], required: true })
   private gifs!: GifObj[];
 }
