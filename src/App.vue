d077b9e3a7b357f703c7c3f3cb2364e51c398452
PozNas1 <anastasia.pazniak@gmail.com>
2021-07-22 14:13:04 +0300

    Add router links to the App.

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,26 @@
 <template>
   <div id="app">
-    <Gifs msg="Welcome" :gifs="gifInfo"/>
-    <div> {{tags}}</div>
+    <div id="nav">
+      <h1> Gifs </h1>
+      <router-link to="/">Popular</router-link> |
+      <router-link to="/search">Search</router-link> 
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script lang="ts">
-import axios from 'axios';
-import { Component, Vue } from 'vue-property-decorator';
-import Gifs from './components/Gifs.vue';
+<style lang="scss">
 
-export interface GifObj {
-  link: string;
-  id: string;
-}
+#nav {
+  padding: 30px;
 
-interface GifData {
-  tags: GifObj[];
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-@Component({
-  components: {
-    Gifs,
-  },
-})
-export default class App extends Vue {
-  tags = '';
-  gifInfo: GifObj[] = [];
-
-  mounted(): void {
-    this.getGifs();
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
-
-  isGifData(obj: unknown): obj is GifData {
-    return typeof obj === "object" 
-      && obj != null
-      && ["tags"].every(i => i in obj)
-  }
-
-  async getGifs(): Promise<void>{ 
-    await axios.get('https://api.gfycat.com/v1/reactions/populated')
-      .then(data => {
-        this.tags = 'Hello';
-
-        if (this.isGifData(data.data)) {
-          this.gifInfo = data.data.tags.map((tag: any):GifObj => {
-            return {link: tag.gfycats[0].gif100px, 
-                        id: tag.tagText}
-          });
-          console.log(this.gifInfo)
-        } else {
-          throw console.error('Type of data is not valid');
-        }
-        
-      })
-      .catch(err => console.log(err));
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 10px;
 }
 </style>
