b8748e2b44bd14a1da15e7b4d1eb05e1182d0029
PozNas1 <anastasia.pazniak@gmail.com>
2021-07-26 14:58:22 +0100

    Adjust pages according to store.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script lang="ts">
-import axios from 'axios';
 import { Component, Vue } from 'vue-property-decorator';
-import { GifObj, GifData } from '../model/interfaces';
+import { GifObj } from '../model/interfaces';
 import Gifs from '../components/Gifs.vue';
+import store from '../store';
 
 @Component({
   name: 'Home',
@@ -17,28 +17,12 @@ import Gifs from '../components/Gifs.vue';
   },
 })
 export default class Home extends Vue {
+
   gifInfo: GifObj[] = [];
+
   mounted(): void {
-    this.getGifs();
-  }
-  isGifData(obj: unknown): obj is GifData {
-    return typeof obj === "object" 
-      && obj != null
-      && ["tags"].every(i => i in obj)
-  }
-  async getGifs(): Promise<void>{ 
-    await axios.get('https://api.gfycat.com/v1/reactions/populated')
-      .then(data => {
-        if (this.isGifData(data.data)) {
-          this.gifInfo = data.data.tags.map((tag: any):GifObj => {
-            return {link: tag.gfycats[0].gif100px, 
-                        id: tag.gfycats[0].tags}
-          });
-        } else {
-          throw console.error('Type of data is not valid');
-        }
-      })
-      .catch(err => console.log(err));
+    this.$store.dispatch('getGifs');
+    this.gifInfo = this.$store.state;
   }
 }
 </script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -20,8 +20,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Gifs from '../components/Gifs.vue';
-import { GifObj, GifData } from '../model/interfaces';
-import axios from 'axios';
+import { GifObj } from '../model/interfaces';
 
 @Component({
   name: 'Search',
@@ -40,30 +39,13 @@ export default class Search extends Vue {
 
   searchInput = '';
 
-
   gifInfo: GifObj[] = [];
+
   mounted(): void {
-    this.getGifs();
+    this.$store.dispatch('getGifs');
+    this.gifInfo = this.$store.state;
   }
-  isGifData(obj: unknown): obj is GifData {
-    return typeof obj === "object" 
-      && obj != null
-      && ["tags"].every(i => i in obj)
-  }
-  async getGifs(): Promise<void>{ 
-    await axios.get('https://api.gfycat.com/v1/reactions/populated')
-      .then(data => {
-        if (this.isGifData(data.data)) {
-          this.gifInfo = data.data.tags.map((tag: any):GifObj => {
-            return {link: tag.gfycats[0].gif100px, 
-                        id: tag.gfycats[0].tags}
-          });
-        } else {
-          throw console.error('Type of data is not valid');
-        }
-      })
-      .catch(err => console.log(err));
-  }
+
 }
 </script>
 
